2edcb62681f59710201fac93535bd7d44d9d4c97
katarighe <m.ighe@hotmail.com>
2023-11-23 15:36:43 +0300

    Add the mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'smart-pocket-app.onrender.com' }
+  config.action_mailer.default_url_options = { :host => 'budget-app-trxc.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
