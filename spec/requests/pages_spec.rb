1cdb89466e27a67fd7cc1db73762b7cef48f6b84
katarighe <m.ighe@hotmail.com>
2023-11-24 05:01:07 +0300

    Fix typo error

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Pages', type: :request do
   before :each do
-    user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
+    @user = User.create(name: 'Tom', email: 'tom@example.com', password: 'topsecret')
     login(@user)
   end
 
